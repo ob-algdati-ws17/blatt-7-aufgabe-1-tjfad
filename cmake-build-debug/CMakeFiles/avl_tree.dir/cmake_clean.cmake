bf73f084fccd58caf1281d7248c4bb6ac8160037
chris <hahn.christoph@gmx.de>
2018-01-03 14:05:28 +0100

    first commit

---
+++ cmake-build-debug/CMakeFiles/avl_tree.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/avl_tree.dir/library.cpp.o"
+  "libavl_tree.pdb"
+  "libavl_tree.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/avl_tree.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ library.h
@@ -0,0 +1,6 @@
+#ifndef AVL_TREE_LIBRARY_H
+#define AVL_TREE_LIBRARY_H
+
+void hello();
+
+#endif
